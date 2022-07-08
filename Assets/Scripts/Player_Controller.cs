97f42a0c71981029571b8aa70c2e9d4d801cd29a
steve rector <steve.rector@datadoghq.com>
2022-07-08 15:55:11 -0700

    Adds Functionality so player can only jump while touching Jumpable layer
    Jumpable layer will be applied to things like platforms and beans to indicate the user should be able to jump again while touching something like that.
    
    Used raycast to detect the layer via a layer mask

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -7,16 +7,21 @@ using UnityEngine.InputSystem;
 public class Player_Controller : MonoBehaviour
 {
     Rigidbody2D _playerRigidBody;
+    BoxCollider2D _playerBoxCollider;
     PlayerControls _playerControls;
+    float _moveInputXValue;    // x value returned from Vector2 when player inputs move action
+
 
     [Header("Move Speed and Jump Height")]
     [Tooltip("adjusts height of jump")][SerializeField] float jumpHeight = 1f;
     [Tooltip("adjusts speed of movement")][SerializeField] float moveSpeed = 1f;
+    [SerializeField] LayerMask platformLayerMask;
 
 
     private void Awake()
     {
         _playerRigidBody = GetComponent<Rigidbody2D>();
+        _playerBoxCollider = GetComponent<BoxCollider2D>();
     }
 
 
@@ -25,8 +30,20 @@ public class Player_Controller : MonoBehaviour
         _playerControls = new PlayerControls();
         _playerControls.Enable();
 
-        _playerControls.Player.Move.performed += Move;
         _playerControls.Player.Jump.performed += Jump;
+    }
+
+
+    private void Update()
+    {
+        _moveInputXValue = _playerControls.Player.Move.ReadValue<Vector2>().x;
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 
 
@@ -36,14 +53,46 @@ public class Player_Controller : MonoBehaviour
     }
 
 
-    private void Move(InputAction.CallbackContext context)
+    /// <summary>
+    /// Called in FixedUpdate, takes in the players movinput and applies force
+    /// to player based on that input, allows for left and right movement
+    /// </summary>
+    private void Move()
     {
-        throw new NotImplementedException();
+        Vector2 processedMoveVector = new Vector2(_moveInputXValue, 0) * moveSpeed;
+        _playerRigidBody.AddForce(processedMoveVector * Time.deltaTime, ForceMode2D.Force);
     }
 
 
+    /// <summary>
+    /// called any time the jump input action is performed. Applies upward force
+    /// to the players rigidbody
+    /// </summary>
+    /// <param name="context"></param>
     private void Jump(InputAction.CallbackContext context)
     {
-        _playerRigidBody.AddForce(Vector2.up * jumpHeight);
+        if (IsGrounded())
+        {
+            _playerRigidBody.AddForce(Vector2.up * jumpHeight);
+        }
+    }
+
+
+    private bool IsGrounded()
+    {
+        float extraHeightTest = .02f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(_playerBoxCollider.bounds.center, Vector2.down, _playerBoxCollider.bounds.extents.y + extraHeightTest, platformLayerMask);
+        Color rayColor;
+        if(raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+
+        Debug.DrawRay(_playerBoxCollider.bounds.center, Vector2.down * (_playerBoxCollider.bounds.extents.y + extraHeightTest), rayColor);
+        return raycastHit.collider != null;
     }
 }
