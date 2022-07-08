f1184691374497e50518129eae5adae63684a556
steve rector <steve.rector@datadoghq.com>
2022-07-08 12:13:24 -0700

    Adds player ability to jump
    Player can now jump with new input system (bound to spacebar on keyboard)
    
    SteveSandbox scene added for safe testing

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Controller : MonoBehaviour
+{
+    Rigidbody2D _playerRigidBody;
+    PlayerControls _playerControls;
+
+    [Header("Move Speed and Jump Height")]
+    [Tooltip("adjusts height of jump")][SerializeField] float jumpHeight = 1f;
+    [Tooltip("adjusts speed of movement")][SerializeField] float moveSpeed = 1f;
+
+
+    private void Awake()
+    {
+        _playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnEnable()
+    {
+        _playerControls = new PlayerControls();
+        _playerControls.Enable();
+
+        _playerControls.Player.Move.performed += Move;
+        _playerControls.Player.Jump.performed += Jump;
+    }
+
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        _playerRigidBody.AddForce(Vector2.up * jumpHeight);
+    }
+}
