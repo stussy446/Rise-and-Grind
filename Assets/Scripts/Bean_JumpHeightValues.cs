c105b66b1314a1e3742598c118ef24720e35f38f
steve rector <steve.rector@datadoghq.com>
2022-07-10 18:47:56 -0700

    Adds logic for consistent boost jumps
    Player boosts are now the same regardless of what part of the bean is collided with.
    
    Also rewrote boost logic so the amount that the player will be boosted will be based off of each beans "boostSpeeed" value, each bean will have a Bean_JumpHeightValues script that holds this information

---
+++ Assets/Scripts/Bean_JumpHeightValues.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bean_JumpHeightValues : MonoBehaviour
+{
+    [SerializeField] float boostSpeed = 1f;
+
+    public float GetBoostSpeed() { return boostSpeed;}
+}
--- Assets/Scripts/Player_Booster.cs
+++ Assets/Scripts/Player_Booster.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class Player_Booster : MonoBehaviour
 {
-    [SerializeField] float boostSpeed = 1f;
-    [SerializeField] int beanLayerIndex = 8;
+    [SerializeField] int beanLayerIndex = 8; // ideally want to find a better way to do this if time
+
     Rigidbody2D _playerRigidBody;
+    Bean_JumpHeightValues bean;
+    Vector2 boostVector = new Vector2(0, 1); // starting vector before it is multiples by boostSpeed
+    
 
-
-    void Awake()
+    void Start()
     {
         _playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
 
     /// <summary>
-    /// if the player collides with a bean, give player a vertical boost
+    /// if the player collides with a bean, give player a vertical boost based
+    /// on that beans boostspeed bonus
     /// </summary>
     /// <param name="collision"></param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == beanLayerIndex)
         {
-            _playerRigidBody.AddForce(Vector2.up * boostSpeed);
+            bean = collision.gameObject.GetComponent<Bean_JumpHeightValues>();
+            _playerRigidBody.velocity= boostVector * bean.GetBoostSpeed();
         }
     }
 
