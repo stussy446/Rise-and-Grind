7ab9b595e67d76a9cb27c140f7c82fc24d278539
steve rector <steve.rector@datadoghq.com>
2022-07-26 12:35:27 -0700

    Projectiles shooting forward
    projectiles successfully shoot forward relative to player

--- Assets/Scripts/Player Functionality/ProjectileSpawner.cs
+++ Assets/Scripts/Player Functionality/ProjectileSpawner.cs
@@ -6,13 +6,17 @@ public class ProjectileSpawner : MonoBehaviour
 {
 
     [SerializeField] GameObject[] projectiles;
-    [SerializeField] float shotSpeed = 1f;
+    [SerializeField] float xShotSpeed = 1f;
+    [SerializeField] float yShotSpeed = 1f;
+
 
 
     public void FireProjectile()
     {
         int projectileToSpawn = Random.Range(0, projectiles.Length - 1);
-        GameObject projectile = Instantiate(projectiles[projectileToSpawn], transform.position, Quaternion.identity);
+        GameObject projectile = Instantiate(projectiles[projectileToSpawn], transform.position, transform.rotation);
+        Vector3 shotForce = new Vector3(xShotSpeed, yShotSpeed, 0);
+        projectile.GetComponent<Rigidbody2D>().AddRelativeForce(shotForce);
     }
 
 
