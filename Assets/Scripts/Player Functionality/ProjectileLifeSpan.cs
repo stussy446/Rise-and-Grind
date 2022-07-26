423f2b9268d681a693c57a6a58fedec29f6431aa
steve rector <steve.rector@datadoghq.com>
2022-07-26 12:44:54 -0700

    Projectiles Deactivate after set amount of time
    To avoid the projectiles staying on the screen too long. Stretch goal is to implement object pooling to reduce performance load with this

---
+++ Assets/Scripts/Player Functionality/ProjectileLifeSpan.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLifeSpan : MonoBehaviour
+{
+    [SerializeField] float secondsUntilDeactivation = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeactivateProjectile());
+    }
+
+    IEnumerator DeactivateProjectile()
+    {
+        yield return new WaitForSeconds(secondsUntilDeactivation);
+        gameObject.SetActive(false);
+    }
+}
