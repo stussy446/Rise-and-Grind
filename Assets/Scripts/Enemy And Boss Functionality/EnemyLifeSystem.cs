3b32977a4333066b573788d041124aff1b20718a
steve rector <steve.rector@datadoghq.com>
2022-07-26 13:10:02 -0700

    Projectiles destroy enemies and deactivate on enemy/boss collission
    need to alter boss to take damage from projectiles instead of the player jumping on her head

---
+++ Assets/Scripts/Enemy And Boss Functionality/EnemyLifeSystem.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLifeSystem : MonoBehaviour, ILifeSystem
+{
+
+    [Tooltip("amount of hp object has")][SerializeField] float healthPoints = 1f;
+    [Tooltip("holds reference to ground layer")][SerializeField] LayerMask groundLayer;
+    [SerializeField] float deathLength = .25f;
+
+
+    public void TakeDamage()
+    {
+        healthPoints -= 1f;
+        // play damage sound 
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == groundLayer) { return; }
+
+        if (GetComponent<PolygonCollider2D>().IsTouchingLayers(LayerMask.GetMask("Projectile")))
+        {
+            TakeDamage();
+        }
+
+        if (healthPoints <= float.Epsilon)
+        {
+            Die();
+        }
+    }
+
+
+    public void Die()
+    {
+        // set up animation stuff
+        // play death sound 
+        Debug.Log("enemy died");
+        StartCoroutine(DeathSequence());
+    }
+
+    public IEnumerator DeathSequence()
+    {
+        yield return new WaitForSeconds(deathLength);
+        // death stuff (animations etc)
+        gameObject.SetActive(false);
+    }
+
+
+
+
+  
+}
--- Assets/Scripts/Player Functionality/ProjectileLifeSpan.cs
+++ Assets/Scripts/Player Functionality/ProjectileLifeSpan.cs
@@ -13,6 +13,19 @@ public class ProjectileLifeSpan : MonoBehaviour
         StartCoroutine(DeactivateProjectile());
     }
 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (GetComponent<BoxCollider2D>().IsTouchingLayers(LayerMask.GetMask("Enemies"))
+               || GetComponent<BoxCollider2D>().IsTouchingLayers(LayerMask.GetMask("Boss")))
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+    }
+
+
     IEnumerator DeactivateProjectile()
     {
         yield return new WaitForSeconds(secondsUntilDeactivation);
