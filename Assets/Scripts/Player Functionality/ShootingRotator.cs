6aa865ebb000b1058b63ea50b6a0b6349e3662d3
steve rector <steve.rector@datadoghq.com>
2022-08-08 11:09:22 -0700

    implements touch rotation/builds on phone
    yay

--- Assets/Scripts/Player Functionality/ShootingRotator.cs
+++ Assets/Scripts/Player Functionality/ShootingRotator.cs
@@ -7,6 +7,7 @@ public class ShootingRotator : MonoBehaviour
 
     private Camera mainCam;
     private Vector3 mousePos;
+    private Vector3 touchPos;
     [SerializeField] Transform playerTransform;
     [SerializeField] GameObject crossHairs;
 
@@ -25,14 +26,29 @@ public class ShootingRotator : MonoBehaviour
 
     void Rotate()
     {
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+            crossHairs.transform.position = new Vector2(mousePos.x, mousePos.y);
 
-        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-        crossHairs.transform.position = new Vector2(mousePos.x, mousePos.y);
+            Vector3 difference = mousePos - transform.position;
+            float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
-        Vector3 difference = mousePos - transform.position;
-        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+        }
+        else if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, transform.position.z));
+            crossHairs.transform.position = new Vector2(touch.position.x, touch.position.y);
 
-        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+            Vector3 difference = (Vector3)touch.position - transform.position;
+            float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
+            transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+        }
     }
+       
+
 }
+
