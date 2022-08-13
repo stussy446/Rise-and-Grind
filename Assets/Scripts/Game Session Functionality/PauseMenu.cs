b5868ec9c5adee24f8d7615f0483354cf0de08eb
steve rector <steve.rector@datadoghq.com>
2022-08-12 17:14:52 -0700

    Sets Up working pause menu in Steve Sandbox
    All buttons work as intended, not saved into prefab yet as the UI Sizing needs to get sorted first

---
+++ Assets/Scripts/Game Session Functionality/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    GameObject pauseMenu;
+
+    private void Awake()
+    {
+        pauseMenu = GameObject.FindGameObjectWithTag("Pause Menu");
+        pauseMenu.SetActive(false);
+    }
+
+ 
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+    public void Quit()
+    {
+        Debug.Log("quitting");
+        Debug.Log("Maybe go to main menu scene if we  end up having one instead of just quitting?");
+        Application.Quit();
+    }
+
+   
+}
