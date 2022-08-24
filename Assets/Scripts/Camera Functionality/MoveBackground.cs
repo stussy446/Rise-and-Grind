ae4814ca039e99e6cb1b54ea839defff9236ec00
Maggie Harden <maggiehawrites@gmail.com>
2022-08-24 18:28:23 -0400

    Added MoveBackground script for parallax implementation
    Worked on EZ Level 1 as well

--- Assets/Scripts/Camera Functionality/MoveBackground.cs
+++ Assets/Scripts/Camera Functionality/MoveBackground.cs
@@ -1,22 +1,23 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveBackground : MonoBehaviour
 {
     private float startPos;
-    private GameObject cam;
+    private GameObject MainCamera;
     [SerializeField] private float parallaxEffect;
 
     void Start()
     {
-        cam = GameObject.Find("MainCamera");
+        MainCamera = GameObject.Find("MainCamera");
         startPos = transform.position.y;
     }
 
     void Update()
     {
-        float distance = (cam.transform.position.y * parallaxEffect);
+        float distance = (MainCamera.transform.position.y * parallaxEffect);
         transform.position = new Vector3(startPos + distance, transform.position.x, transform.position.z);
     }
 }
+*/
